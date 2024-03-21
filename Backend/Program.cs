72647ef3874dbf759c02476631895975014a749e
Andre Baltieri <balta@balta-macbook-pro-16.local>
2024-03-21 11:32:49 -0300

    Finalizado demo

---
+++ Backend/Program.cs
@@ -0,0 +1,153 @@
+using System.Data;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Establish cookie authentication
+builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+
+// Configure app cookie
+//
+// The default values, which are appropriate for hosting the Backend and
+// BlazorWasmAuth apps on the same domain, are Lax and SameAsRequest. 
+// For more information on these settings, see:
+// https://learn.microsoft.com/aspnet/core/blazor/security/webassembly/standalone-with-identity#cross-domain-hosting-same-site-configuration
+/*
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+});
+*/
+
+// Configure authorization
+builder.Services.AddAuthorizationBuilder();
+
+// Add the database (in memory for the sample)
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        options.UseInMemoryDatabase("AppDb");
+        //For debugging only: options.EnableDetailedErrors(true);
+        //For debugging only: options.EnableSensitiveDataLogging(true);
+    });
+
+// Add identity and opt-in to endpoints
+builder.Services.AddIdentityCore<AppUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
+
+// Add a CORS policy for the client
+builder.Services.AddCors(
+    options => options.AddPolicy(
+        "wasm",
+        policy => policy.WithOrigins([builder.Configuration["BackendUrl"] ?? "https://localhost:5001", 
+            builder.Configuration["FrontendUrl"] ?? "https://localhost:5002"])
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()));
+
+// Add services to the container
+builder.Services.AddEndpointsApiExplorer();
+
+// Add NSwag services
+builder.Services.AddOpenApiDocument();
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    // Seed the database
+    await using var scope = app.Services.CreateAsyncScope();
+    await SeedData.InitializeAsync(scope.ServiceProvider);
+
+    // Add OpenAPI/Swagger generator and the Swagger UI
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
+// Create routes for the identity endpoints
+app.MapIdentityApi<AppUser>();
+
+// Activate the CORS policy
+app.UseCors("wasm");
+
+// Enable authentication and authorization after CORS Middleware
+// processing (UseCors) in case the Authorization Middleware tries
+// to initiate a challenge before the CORS Middleware has a chance
+// to set the appropriate headers.
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Provide an end point to clear the cookie for logout
+//
+// For more information on the logout endpoint and antiforgery, see:
+// https://learn.microsoft.com/aspnet/core/blazor/security/webassembly/standalone-with-identity#antiforgery-support
+app.MapPost("/logout", async (SignInManager<AppUser> signInManager, [FromBody] object empty) =>
+{
+    if (empty is not null)
+    {
+        await signInManager.SignOutAsync();
+
+        return Results.Ok();
+    }
+
+    return Results.Unauthorized();
+}).RequireAuthorization();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/roles", (ClaimsPrincipal user) =>
+{
+    if (user.Identity is not null && user.Identity.IsAuthenticated)
+    {
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity.FindAll(identity.RoleClaimType)
+            .Select(c => 
+                new
+                {
+                    c.Issuer, 
+                    c.OriginalIssuer, 
+                    c.Type, 
+                    c.Value, 
+                    c.ValueType
+                });
+
+        return TypedResults.Json(roles);
+    }
+
+    return Results.Unauthorized();
+}).RequireAuthorization();
+
+app.MapPost("/data-processing-1", ([FromBody] FormModel model) =>
+    Results.Text($"{model.Message.Length} characters"))
+        .RequireAuthorization();
+
+app.MapPost("/data-processing-2", ([FromBody] FormModel model) =>
+    Results.Text($"{model.Message.Length} characters"))
+        .RequireAuthorization(policy => policy.RequireRole("Manager"));
+
+app.Run();
+
+// Identity user
+class AppUser : IdentityUser
+{
+    public IEnumerable<IdentityRole>? Roles { get; set; }
+}
+
+// Identity database
+class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<AppUser>(options)
+{
+}
+
+// Example form model
+class FormModel
+{
+    public string Message { get; set; } = string.Empty;
+}
